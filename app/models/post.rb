f71d0b479a2b309ee5109efcdd63f08605f1784f
Colombianito66 <colombianito66@gmail.com>
2017-02-08 04:33:31 -0500

    modified model / schema to include image upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+
+validates :title, presence: true, length: { minimum: 4 }
+validates :body, presence: true
+
+has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  
 end
